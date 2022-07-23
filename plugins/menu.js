856ac4e2353649eaa2636d13a0da566526b7e352
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2022-07-24 02:27:20 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -9,10 +9,9 @@ exports.run = {
          rowId: `${isPrefix}menutype 1`,
          description: ``
       }]
-      let text = '*HanBotzX* adalah sistem otomatis WhatsApp (WhatsApp Bot) yang dapat membantu untuk melakukan penjagaan terhadap grup anda. _free add to your group_\n\n'
-      text += '◦ *Donate* : https://saweria.co/HanBotz\n'
-      text += '◦ *Group* : https://chat.whatsapp.com/KBxslpQTy08Djs32qK2TJQ\n\n'
-      text += 'Fitur lainnya di https://hanbotz.xyz'
+      let text = '• *Group* : https://chat.whatsapp.com/KBxslpQTy08Djs32qK2TJQ\n'
+      text += '• *Donate* : https://saweria.co/HanBotz\n\n'
+      text += 'Fitur lainnya di hanbotz.xyz'
       await client.sendList(m.chat, '', text, '© 2022 HanBotzX', 'Tap!', [{
          rows
       }], m)
